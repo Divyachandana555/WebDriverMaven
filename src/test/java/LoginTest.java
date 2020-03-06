42218477a9cdee433435256e51e5b983287d1255
HR <HR@INHYNITPOOL164.infor.com>
2020-03-06 19:51:04 +0530

    Initial Commit

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,39 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTest {
+
+	public static WebDriver driver;
+
+	@BeforeSuite
+	public void setUp() {
+		
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+
+	}
+
+	@Test
+	public void doLogin() {
+
+		driver.get("http://gmail.com");
+		driver.findElement(By.id("identifierId")).sendKeys("divyapeddakotla@gmail.com");
+		driver.findElement(By.xpath("//span[@class='RveJvd snByac']")).click();
+	}
+
+	@AfterSuite
+	public void tearDown() {
+
+		driver.quit();
+
+	}
+
+}
